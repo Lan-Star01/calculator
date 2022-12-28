979df50366dca4d6c46b1dba934803efcaa07648
Lana Mezvrishvili <lana.mezvrishvili.08@gmail.com>
2022-12-28 21:26:25 +0400

    calculator

---
+++ src/components/lightMode/LightMode.js
@@ -0,0 +1,44 @@
+import './LightModeStyle.css'
+import {useReducer} from "react";
+import reducer, {ACTIONS} from "./Functionality";
+import DigitButton from "./DigitButton";
+import OperationButton from "./OperationButton";
+
+
+function LightMode() {
+
+    const [{currentOperand, previousOperand, operation}, dispatch] = useReducer(reducer,{});
+
+    return(
+
+        <div className="container">
+            <div className="output">
+                <div className="previous-operand">{previousOperand} {operation}</div>
+                <div className="current-operand">{currentOperand}</div>
+            </div>
+
+            <div className="calculator-grid">
+                <button className="span-two" onClick={() => dispatch({type: ACTIONS.CLEAR})}>AC</button>
+                <button onClick={() => dispatch({type: ACTIONS.DELETE_DIGIT})}>D</button>
+                <OperationButton operation="÷" dispatch={dispatch}/>
+                <DigitButton digit="1" dispatch={dispatch}/>
+                <DigitButton digit="2" dispatch={dispatch}/>
+                <DigitButton digit="3" dispatch={dispatch}/>
+                <OperationButton operation="x" dispatch={dispatch}/>
+                <DigitButton digit="4" dispatch={dispatch}/>
+                <DigitButton digit="5" dispatch={dispatch}/>
+                <DigitButton digit="6" dispatch={dispatch}/>
+                <OperationButton operation="+" dispatch={dispatch}/>
+                <DigitButton digit="7" dispatch={dispatch}/>
+                <DigitButton digit="8" dispatch={dispatch}/>
+                <DigitButton digit="9" dispatch={dispatch}/>
+                <OperationButton operation="-" dispatch={dispatch}/>
+                <DigitButton digit="0" dispatch={dispatch}/>
+                <DigitButton digit="." dispatch={dispatch}/>
+                <button className="span-two" onClick={() => dispatch({type: ACTIONS.EVALUATE})}>=</button>
+            </div>
+        </div>
+
+    )
+}
+export default LightMode;
